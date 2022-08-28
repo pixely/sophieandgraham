fdc9930e47b3f2b35d85bb63bdca018b6e46a7fa
Graham Martin <graham@pixely.co.uk>
2022-08-28 23:42:44 +0100

    feat: image compression

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,46 @@ module.exports = function(eleventyConfig) {
 		files: './_site/css/**/*.css'
 	});
 
+  
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
+
+  const Image = require("@11ty/eleventy-img");
+
+  async function imageShortcode(src, alt, sizes = "100vw", widths = "300,600", className = '') {
+    if(alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on responsiveimage from: ${src}`);
+    }
+
+    let metadata = await Image(src, {
+      widths: widths.split(','),
+      formats: ["avif", "webp", "jpeg"],
+      outputDir: "./_site/images/",
+      urlPath: "/images/",
+    });
+
+    let lowsrc = metadata.jpeg[0];
+    let highsrc = metadata.jpeg[metadata.jpeg.length - 1];
+
+    return `<picture>
+      ${Object.values(metadata).map(imageFormat => {
+        return `  <source type="${imageFormat[0].sourceType}" srcset="${imageFormat.map(entry => entry.srcset).join(", ")}" sizes="${sizes}">`;
+      }).join("\n")}
+        <img
+          src="${lowsrc.url}"
+          width="${highsrc.width}"
+          height="${highsrc.height}"
+          alt="${alt}"
+          class="${className}"
+          loading="lazy"
+          decoding="async">
+      </picture>`;
+  }
+
+  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addLiquidShortcode("image", imageShortcode);
+  eleventyConfig.addJavaScriptFunction("image", imageShortcode);
+
 
   return {
     dir: {
---
+++ src/_data/images.js
@@ -0,0 +1,32 @@
+const Image = require('@11ty/eleventy-img');
+
+// Get all site information
+module.exports = async function () {
+    const defaults = {
+        formats: ["avif", "webp", "jpeg"],
+        outputDir: "./_site/images/",
+    };
+
+    // const images = [{
+    //     name: "sophie-graham",
+    //     url: "./src/assets/sophie-graham.jpg",
+    //     widths: [300],
+    // }];
+
+    const images = [];
+
+    const resized = {};
+
+    await images.forEach(async image => {
+        console.log('image', image);
+        resized[image.name] = 
+            await Image(image.url, {
+                ...defaults,
+                widths: image.widths,
+            });
+    });
+
+    return {
+        'images': resized,
+    };
+};
