eed54328b159fafa1e5dab0f346b1a784527e08f
Graham Martin <graham@pixely.co.uk>
2022-12-29 14:42:29 +0000

    fix: typos

--- src/js/modules/rsvp.js
+++ src/js/modules/rsvp.js
@@ -1,3 +1,20 @@
 export function init() {
-    console.log('rsvp form');   
+    const continueButton = document.querySelector('.js-continue');
+    const rsvpSelects = document.querySelectorAll('.js-rsvp-select input');
+
+    console.log('rsvp form');
+    // show continue button
+    rsvpSelects.forEach((select) => {
+        select.addEventListener('change', (event) => {
+            console.log(event);
+            console.log(event.target.value);
+            continueButton.removeAttribute('disabled');
+        });
+    });
+
+    // show meal choice section on press of continue button
+
+    // show meal choice only for accepting guests
+    
+    // show confirm button
 }
