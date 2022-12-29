3753aaa657424e0a1cab9fbcf0a434eff1a365a3
Graham Martin <graham@pixely.co.uk>
2022-12-29 12:43:18 +0000

    feat: add basic js assets

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,9 @@
 import '../sass/app.scss';
+
+const dynamicModules = [...document.querySelectorAll('[data-module]')].map((module) => module.dataset.module);
+
+if (dynamicModules.includes('rsvp')) {
+    import("./modules/rsvp").then(function (rsvp) {
+        rsvp.init();
+    });
+}
